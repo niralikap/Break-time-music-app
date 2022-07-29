a37d79fa78990ee9d4e01e6c19d65e7932c25224
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2022-07-29 07:46:51 -0700

    I updated some code in HTML, stored the restaurant list data to localstorge by requirement, still working on display the food name and food videos on the bowser. Currently, we can see the food name and video inside the console

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,9 @@ var restaurant2El = document.querySelector('#restaurants-2');
 var restaurant3El = document.querySelector('#restaurants-3');
 var restaurant4El = document.querySelector('#restaurants-4');
 var restaurant5El = document.querySelector('#restaurants-5');
-//Update somthing
+
+var videoListEl = document.querySelector('#video-list');
+
 function getRestaurants (zipcode) {
     const options = {
         method: 'GET',
@@ -26,32 +28,36 @@ function getRestaurants (zipcode) {
         })
         .then(function(data){
             // console.log(data);
-            
-            restaurant1El.textContent = data.restaurants[1].restaurantName;
-            restaurant2El.textContent = data.restaurants[2].restaurantName;
-            restaurant3El.textContent = data.restaurants[3].restaurantName;
-            restaurant4El.textContent = data.restaurants[4].restaurantName;
-            restaurant5El.textContent = data.restaurants[5].restaurantName;
+            var restaurantList = {
+                listRestaurant1: data.restaurants[1].restaurantName,
+                listRestaurant2: data.restaurants[2].restaurantName,
+                listRestaurant3: data.restaurants[3].restaurantName,
+                listRestaurant4: data.restaurants[4].restaurantName,
+                listRestaurant5: data.restaurants[5].restaurantName,
+            } 
+            localStorage.setItem('restaurantList', JSON.stringify(restaurantList));
+            renderRestaurants();
         })
-}
+    }
+    
+    function renderRestaurants() {
+        var localRestaurant = JSON.parse(localStorage.getItem('restaurantList'));
+        restaurant1El.textContent = localRestaurant.listRestaurant1;
+        restaurant2El.textContent = localRestaurant.listRestaurant2;
+        restaurant3El.textContent = localRestaurant.listRestaurant3;
+        restaurant4El.textContent = localRestaurant.listRestaurant4;
+        restaurant5El.textContent = localRestaurant.listRestaurant5;
+    }
 
 function getVideos() {
-    // const options = {
-    //     method: 'GET',
-    //     headers: {
-    //         'X-RapidAPI-Key': '77c8fec0d0mshdb05a13cd8505e8p1c1508jsn238f4fdfc268',
-    //         'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-    //     }
-    // };
-    // fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes', options)
-    const options = {
+    const optionVideos = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': '77c8fec0d0mshdb05a13cd8505e8p1c1508jsn238f4fdfc268',
             'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
         }
     };
-    fetch('https://tasty.p.rapidapi.com/recipes/list-similarities?recipe_id=8138', options)
+    fetch('https://tasty.p.rapidapi.com/recipes/list-similarities?recipe_id=8138', optionVideos)
     .then(function (response) {
         if(response.ok){
             return response.json();
@@ -64,7 +70,13 @@ function getVideos() {
             // console.log(i)
             if(data.results[i].video_url !== null) {
                 console.log(data.results[i].name);
-                console.log(data.results[i].video_url, i);
+                // console.log(data.results[i].video_url, i);
+                var vN = document.createElement('li');
+                vN.innerHTML = data.results[i].name
+                // var videoNameEl = `<li ${data.results[i].name}></li>`;
+                // videoNameEl.innerHTML = videoNameEl;
+                // var videoEl = data.results[i].video_url;
+                
             }
         }  
     })
@@ -83,6 +95,6 @@ restaurantBtnContEl.addEventListener('click', function(){
     getVideos()
 });
 
-
+renderRestaurants();
 
 
