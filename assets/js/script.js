6c75d027c5538b78f5b08b4e8f8dd6d3d06db41b
Nilu <nirali.rkapadia@gmail.com>
2022-07-28 21:07:00 -0700

    Updates the js file with the qury url

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,7 +53,7 @@ fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_mi
           .catch(function (error) {
             alert('Unable to connect to GitHub');
           });
-      };
+      };*/
       
     
       var displayRestaurants = function (restaurants) {
@@ -102,13 +102,13 @@ function getRestaurants (zipcode) {
           .then(response => console.log(response))
           .catch(err => console.error(err));*/
 
-          const options = {
+          /*const options = {
             method: 'GET',
             headers: {
               'X-RapidAPI-Key': '77c8fec0d0mshdb05a13cd8505e8p1c1508jsn238f4fdfc268',
               'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
             }
-          };
+          };*/
           
           fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&name=pizza', options)
             .then(response => response.json())
@@ -155,47 +155,47 @@ fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_mi
     .then(response => console.log(response))
     .catch(err => console.error(err));*/
 
-// var formSubmitHandler = function (event) {
-//         event.preventDefault();
-//         var foodname = foodInputEl.value.trim();
-//         if (foodname) {
-//           getUserRepos(username);
-//           repoContainerEl.textContent = '';
-//           foodInputEl.value = '';
-//         } else {
-//           alert('Please enter a food item');
-//         }
-//       };
+ var formSubmitHandler = function (event) {
+         event.preventDefault();
+         var foodname = foodInputEl.value.trim();
+         if (foodname) {
+           getUserRepos(username);
+           repoContainerEl.textContent = '';
+           foodInputEl.value = '';
+         } else {
+           alert('Please enter a food item');
+         }
+       };
 
-// var getRestaurants = function (food) {
-//         var apiUrl = 'https://api.github.com/users/' + user + '/repos';
-//         fetch(apiUrl)
-//           .then(function (response) {
-//             if (response.ok) {
-//               console.log(response);
-//               response.json().then(function (data) {
-//                 console.log(data);
-//                 displayRepos(data, user);
-//               });
-//             } else {
-//               alert('Error: ' + response.statusText);
-//             }
-//           })
-//           .catch(function (error) {
-//             alert('Unable to connect to GitHub');
-//           });
-//       };
-//       var displayRestaurants = function (restaurants) {
-//         if (restaurants.length === 0) {
-//             restaurantListEl.textContent = 'No restaurants found.';
-//             // Without a `return` statement, the rest of this function will continue to run and perhaps throw an error if `repos` is empty
-//             return;
-//           }
-//           /*for (var i = 0; i < 5; i++) {
-//             // The result will be `<github-username>/<github-repository-name>`
-//             var restaurantN = restaurants[i].restaurantName;
-//             restaurantListEl.append(restaurantN);*/
-//             restaurant1El.textContent = ‘Temp: ‘+ lastItem.dTwoTemp + ‘°F’;
+ var getRestaurants = function (food) {
+         var apiUrl = 'https:api.github.com/users/' + user + '/repos';
+         fetch(apiUrl)
+           .then(function (response) {
+             if (response.ok) {
+               console.log(response);
+               response.json().then(function (data) {
+                 console.log(data);
+                 displayRepos(data, user);
+               });
+             } else {
+               alert('Error: ' + response.statusText);
+             }
+           })
+           .catch(function (error) {
+             alert('Unable to connect to GitHub');
+           });
+       };
+       var displayRestaurants = function (restaurants) {
+         if (restaurants.length === 0) {
+             restaurantListEl.textContent = 'No restaurants found.';
+              Without a `return` statement, the rest of this function will continue to run and perhaps throw an error if `repos` is empty
+             return;
+           }
+           for (var i = 0; i < 5; i++) {
+              The result will be `<github-username>/<github-repository-name>`
+             var restaurantN = restaurants[i].restaurantName;
+             restaurantListEl.append(restaurantN);*/
+             restaurant1El.textContent = ‘Temp: ‘+ lastItem.dTwoTemp + ‘°F’;
     // const options = {
     //     method: 'GET',
     //     headers: {
